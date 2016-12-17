b46682bb780b02a41b1126115f56a2faa97bd7b0
Steve Hu <stevehu@gmail.com>
2016-12-16 22:08:03 -0500

    redo code with swgger

---
+++ code/src/main/java/com/networknt/oauth/code/PathHandlerProvider.java
@@ -0,0 +1,20 @@
+package com.networknt.oauth.code;
+
+import com.networknt.oauth.code.handler.Oauth2CodeGetHandler;
+import com.networknt.oauth.code.handler.Oauth2CodePostHandler;
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.util.Methods;
+
+public class PathHandlerProvider implements HandlerProvider {
+
+    public HttpHandler getHandler() {
+        HttpHandler handler = Handlers.routing()
+            .add(Methods.GET, "/v1/oauth2/code", new Oauth2CodeGetHandler())
+            .add(Methods.POST, "/v1/oauth2/code", new Oauth2CodePostHandler())
+        ;
+        return handler;
+    }
+}
+
