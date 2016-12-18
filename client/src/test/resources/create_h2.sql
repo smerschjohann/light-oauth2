824c79734b0040d259b719fb30e0fa3de285b005
Steve Hu <stevehu@gmail.com>
2016-12-18 06:52:41 -0500

    update client

---
+++ client/src/test/resources/create_h2.sql
@@ -0,0 +1,52 @@
+DROP table IF EXISTS users;
+DROP table IF EXISTS  clients;
+DROP table IF EXISTS  services;
+
+create table users (
+  user_id varchar PRIMARY KEY,
+  user_type varchar,  -- admin, customer, employee, partner
+  first_name varchar,
+  last_name varchar,
+  email varchar,
+  password varchar,
+  create_dt DATE,
+  update_dt DATE
+);
+
+CREATE UNIQUE INDEX email_idx ON users(email);
+
+create table clients (
+  client_id VARCHAR PRIMARY KEY,
+  client_type VARCHAR,  -- server, mobile, service, standalone, browser etc
+  client_secret VARCHAR,
+  client_name VARCHAR,
+  client_desc VARCHAR,
+  scope VARCHAR,
+  redirect_url VARCHAR,
+  authenticate_class VARCHAR,
+  owner_id VARCHAR,
+  owner_name VARCHAR,
+  owner_email VARCHAR,
+  create_dt DATE,
+  update_dt DATE
+);
+
+create table services (
+  service_id VARCHAR PRIMARY KEY,
+  service_type VARCHAR,  -- api, ms
+  service_name VARCHAR,
+  service_desc VARCHAR,
+  scope VARCHAR,
+  owner_id VARCHAR,
+  owner_name VARCHAR,
+  owner_email VARCHAR,
+  create_dt DATE,
+  update_dt DATE
+);
+
+INSERT INTO users (user_id, user_type, first_name, last_name, email, password) VALUES('admin', 'admin', 'admin', 'admin', 'admin@networknt.com', 'admin');
+
+INSERT INTO clients (client_id, client_type, client_secret, client_name, client_desc, scope, redirect_url) VALUES('f7d42348-c647-4efb-a52d-4c5787421e72', 'server', 'f6h1FTI8Q3-7UScPZDzfXA', 'PetStore Web Server', 'PetStore Web Server that calls PetStore API', 'petstore.r petstore.w', 'http://localhost:8080/authorization' );
+
+INSERT INTO services (service_id, service_type, service_name, service_desc, scope) VALUES ('AACT0001', 'ms', 'Account Service', 'A microservice that serves account information', 'a.r b.r');
+
