4e38cdd4a1eebae19fa41f0151ca2ae29018e3cb
Steve Hu <stevehu@gmail.com>
2016-12-23 09:11:54 -0500

    password reset tests

--- user/src/main/java/com/networknt/oauth/user/handler/Oauth2PasswordUserIdPostHandler.java
+++ user/src/main/java/com/networknt/oauth/user/handler/Oauth2PasswordUserIdPostHandler.java
@@ -1,6 +1,7 @@
 package com.networknt.oauth.user.handler;
 
 import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
 import com.networknt.oauth.user.HashUtil;
 import com.networknt.oauth.user.PathHandlerProvider;
 import com.networknt.service.SingletonServiceFactory;
@@ -9,10 +10,7 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -27,7 +25,8 @@ public class Oauth2PasswordUserIdPostHandler implements HttpHandler {
 
     static Logger logger = LoggerFactory.getLogger(Oauth2PasswordUserIdPostHandler.class);
     static DataSource ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
-    static String sql = "UPDATE users SET password=? WHERE user_id = ?";
+    static String sqlUpdate = "UPDATE users SET password=? WHERE user_id = ?";
+    static String sqlSelect = "SELECT * FROM users WHERE user_id = ?";
 
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         Map<String, Object> body = (Map)exchange.getAttachment(BodyHandler.REQUEST_BODY);
@@ -35,7 +34,13 @@ public class Oauth2PasswordUserIdPostHandler implements HttpHandler {
         String password = (String)body.get("password");
         String newPassword = (String)body.get("newPassword");
         String newPasswordConfirm = (String)body.get("newPasswordConfirm");
+        if(logger.isDebugEnabled()) {
+            logger.debug("userId:" + userId + " password:" + password + " newPassword:" + newPassword + " newPasswordConfirm:" + newPasswordConfirm);
+        }
         Map<String, Object> user = (Map<String, Object>)PathHandlerProvider.users.get(userId);
+        if(user == null) {
+            user = selectUser(userId);
+        }
         if(!HashUtil.validatePassword(password, (String)user.get("password"))) {
             Status status = new Status(INCORRECT_PASSWORD);
             exchange.setStatusCode(status.getStatusCode());
@@ -45,12 +50,12 @@ public class Oauth2PasswordUserIdPostHandler implements HttpHandler {
         if(newPassword.equals(newPasswordConfirm)) {
             String hashedPass = HashUtil.generateStorngPasswordHash(newPassword);
 
-            try (Connection connection = ds.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql)) {
+            try (Connection connection = ds.getConnection(); PreparedStatement stmt = connection.prepareStatement(sqlUpdate)) {
                 stmt.setString(1, hashedPass);
                 stmt.setString(2, userId);
                 stmt.executeUpdate();
                 user.put("password", hashedPass);
-                PathHandlerProvider.users.put((String)user.get("userId"), user);
+                PathHandlerProvider.users.put(userId, user);
             } catch (SQLException e) {
                 logger.error("Exception:", e);
                 // should handle this exception and return an error message.
@@ -62,5 +67,28 @@ public class Oauth2PasswordUserIdPostHandler implements HttpHandler {
             exchange.getResponseSender().send(status.toString());
             return;
         }
+    }
+
+    Map<String, Object> selectUser(String userId) throws Exception {
+        Map<String, Object> result = null;
+        try (Connection connection = ds.getConnection(); PreparedStatement stmt = connection.prepareStatement(sqlSelect)) {
+            stmt.setString(1, userId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    result = new HashMap<>();
+                    result.put("userId", userId);
+                    result.put("userType", rs.getString("user_type"));
+                    result.put("firstName", rs.getString("first_name"));
+                    result.put("lastName", rs.getString("last_name"));
+                    result.put("email", rs.getString("email"));
+                    result.put("password", rs.getString("password"));
+                    PathHandlerProvider.users.put(userId, result);
+                }
+            }
+        } catch (SQLException e) {
+            logger.error("Exception:", e);
+            throw e;
+        }
+        return result;
     }
 }
