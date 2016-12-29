1b56a99d698b551a12bb87f9c4611d769747c824
Steve Hu <stevehu@gmail.com>
2016-12-29 12:04:28 -0500

    start code post

--- code/src/main/java/com/networknt/oauth/code/PathHandlerProvider.java
+++ code/src/main/java/com/networknt/oauth/code/PathHandlerProvider.java
@@ -15,9 +15,15 @@ import io.undertow.security.handlers.AuthenticationMechanismsHandler;
 import io.undertow.security.handlers.SecurityInitialHandler;
 import io.undertow.security.idm.IdentityManager;
 import io.undertow.security.impl.BasicAuthenticationMechanism;
+import io.undertow.security.impl.CachedAuthenticatedSessionMechanism;
+import io.undertow.security.impl.FormAuthenticationMechanism;
 import io.undertow.server.HttpHandler;
+import io.undertow.server.session.InMemorySessionManager;
+import io.undertow.server.session.SessionAttachmentHandler;
+import io.undertow.server.session.SessionCookieConfig;
 import io.undertow.util.Methods;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,20 +34,32 @@ public class PathHandlerProvider implements HandlerProvider {
         final IdentityManager identityManager = new MapIdentityManager(users);
 
         HttpHandler handler = Handlers.routing()
-            .add(Methods.GET, "/oauth2/code", addSecurity(new Oauth2CodeGetHandler(), identityManager))
-            .add(Methods.POST, "/oauth2/code", new Oauth2CodePostHandler())
+            .add(Methods.GET, "/oauth2/code", addBasicSecurity(new Oauth2CodeGetHandler(), identityManager))
+            .add(Methods.POST, "/oauth2/code", addFormSecurity(new Oauth2CodePostHandler(), identityManager))
         ;
         return handler;
     }
 
-    private static HttpHandler addSecurity(final HttpHandler toWrap, final IdentityManager identityManager) {
+    private static HttpHandler addBasicSecurity(final HttpHandler toWrap, final IdentityManager identityManager) {
         HttpHandler handler = toWrap;
         handler = new AuthenticationCallHandler(handler);
         handler = new AuthenticationConstraintHandler(handler);
-        final List<AuthenticationMechanism> mechanisms = Collections.singletonList(new BasicAuthenticationMechanism("My Realm"));
+        final List<AuthenticationMechanism> mechanisms = Collections.singletonList(new BasicAuthenticationMechanism("oauth2"));
         handler = new AuthenticationMechanismsHandler(handler, mechanisms);
         handler = new SecurityInitialHandler(AuthenticationMode.PRO_ACTIVE, identityManager, handler);
         return handler;
     }
-}
 
+    private static HttpHandler addFormSecurity(final HttpHandler toWrap, final IdentityManager identityManager) {
+        HttpHandler handler = toWrap;
+        handler = new AuthenticationCallHandler(handler);
+        handler = new AuthenticationConstraintHandler(handler);
+        final List<AuthenticationMechanism> mechanisms = new ArrayList<>();
+        mechanisms.add(new CachedAuthenticatedSessionMechanism());
+        mechanisms.add(new FormAuthenticationMechanism("oauth2", "/login", "/error", "/oauth2/code"));
+        handler = new AuthenticationMechanismsHandler(handler, mechanisms);
+        handler = new SecurityInitialHandler(AuthenticationMode.PRO_ACTIVE, identityManager, handler);
+        handler = new SessionAttachmentHandler(handler, new InMemorySessionManager("oauth2"), new SessionCookieConfig());
+        return handler;
+    }
+}
