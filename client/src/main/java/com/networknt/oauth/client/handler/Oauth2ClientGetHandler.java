d012edc21508e091b76a17d7e241cde32d61d40a
Steve Hu <stevehu@gmail.com>
2017-01-04 15:29:28 -0500

    pagination and clientName filter for client service

--- client/src/main/java/com/networknt/oauth/client/handler/Oauth2ClientGetHandler.java
+++ client/src/main/java/com/networknt/oauth/client/handler/Oauth2ClientGetHandler.java
@@ -1,6 +1,12 @@
 package com.networknt.oauth.client.handler;
 
+import com.hazelcast.core.IMap;
+import com.hazelcast.query.PagingPredicate;
+import com.hazelcast.query.impl.predicates.LikePredicate;
 import com.networknt.config.Config;
+import com.networknt.oauth.cache.CacheStartupHookProvider;
+import com.networknt.oauth.cache.model.Client;
+import com.networknt.oauth.cache.model.User;
 import com.networknt.service.SingletonServiceFactory;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
@@ -9,46 +15,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Oauth2ClientGetHandler implements HttpHandler {
     static final Logger logger = LoggerFactory.getLogger(Oauth2ClientGetHandler.class);
-    static final DataSource ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
-    static final String sql = "SELECT * FROM clients";
+
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        List<Map<String, Object>> result = new ArrayList<>();
-        // TODO refactor from map instead of db.
+        IMap<String, Client> clients = CacheStartupHookProvider.hz.getMap("clients");
+        Deque<String> clientNameDeque = exchange.getQueryParameters().get("clientName");
+        String clientName = clientNameDeque == null? "%" : clientNameDeque.getFirst() + "%";
+        int page = Integer.valueOf(exchange.getQueryParameters().get("page").getFirst()) - 1;
+        Deque<String> pageSizeDeque = exchange.getQueryParameters().get("pageSize");
+        int pageSize = pageSizeDeque == null? 10 : Integer.valueOf(pageSizeDeque.getFirst());
 
-        try (Connection connection = ds.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql)) {
-            try (ResultSet rs = stmt.executeQuery()) {
-                while (rs.next()) {
-                    Map<String, Object> client = new HashMap<>();
-                    client.put("clientId", rs.getString("client_id"));
-                    client.put("clientType", rs.getString("client_type"));
-                    client.put("clientName", rs.getString("client_name"));
-                    client.put("clientDesc", rs.getString("client_desc"));
-                    client.put("scope", rs.getString("scope"));
-                    client.put("redirectUrl", rs.getString("redirect_url"));
-                    client.put("authenticateClass", rs.getString("authenticate_class"));
-                    client.put("ownerId", rs.getString("owner_id"));
-                    client.put("createDt", rs.getDate("create_dt"));
-                    client.put("updateDt", rs.getDate("update_dt"));
-                    result.add(client);
-                }
-            }
-        } catch (SQLException e) {
-            logger.error("Exception:", e);
-            throw e;
+        // a comparator which helps to sort in order of clientName field
+        Comparator<Map.Entry> comparator = (Comparator<Map.Entry> & Serializable)(e1, e2) -> {
+            Client c1 = (Client) e1.getValue();
+            Client c2 = (Client) e2.getValue();
+            return c1.getClientName().compareTo(c2.getClientName());
+        };
+
+        LikePredicate likePredicate = new LikePredicate("clientName", clientName);
+
+        PagingPredicate pagingPredicate = new PagingPredicate(likePredicate, comparator, pageSize);
+        pagingPredicate.setPage(page);
+        Collection<Client> values = clients.values(pagingPredicate);
+
+        for (Client value : values) {
+            value.setClientSecret(null);
         }
         exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
-        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(result));
+        exchange.getResponseSender().send(Config.getInstance().getMapper().writeValueAsString(values));
     }
 }
