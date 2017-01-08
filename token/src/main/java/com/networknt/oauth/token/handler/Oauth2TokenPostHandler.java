055b1e78bb852a08f5ae54799c6bed8f1169647d
Steve Hu <stevehu@gmail.com>
2017-01-08 08:28:26 -0500

    fixes #9 add resource owner password credentials grant

--- token/src/main/java/com/networknt/oauth/token/handler/Oauth2TokenPostHandler.java
+++ token/src/main/java/com/networknt/oauth/token/handler/Oauth2TokenPostHandler.java
@@ -18,6 +18,7 @@ import io.undertow.server.handlers.form.FormDataParser;
 import io.undertow.server.handlers.form.FormParserFactory;
 import io.undertow.util.Headers;
 import org.jose4j.jwt.JwtClaims;
+import org.jose4j.lang.JoseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,19 +29,23 @@ import java.util.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Oauth2TokenPostHandler implements HttpHandler {
-    static final Logger logger = LoggerFactory.getLogger(Oauth2TokenPostHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(Oauth2TokenPostHandler.class);
 
-    static final String UNABLE_TO_PARSE_FORM_DATA = "ERR12000";
-    static final String UNSUPPORTED_GRANT_TYPE = "ERR12001";
-    static final String MISSING_AUTHORIZATION_HEADER = "ERR12002";
-    static final String INVALID_AUTHORIZATION_HEADER = "ERR12003";
-    static final String INVALID_BASIC_CREDENTIALS = "ERR12004";
-    static final String JSON_PROCESSING_EXCEPTION = "ERR12005";
-    static final String CLIENT_NOT_FOUND = "ERR12014";
-    static final String UNAUTHORIZED_CLIENT = "ERR12007";
-    static final String INVALID_AUTHORIZATION_CODE = "ERR12008";
-    static final String GENERIC_EXCEPTION = "ERR10014";
-    static final String RUNTIME_EXCEPTION = "ERR10010";
+    private static final String UNABLE_TO_PARSE_FORM_DATA = "ERR12000";
+    private static final String UNSUPPORTED_GRANT_TYPE = "ERR12001";
+    private static final String MISSING_AUTHORIZATION_HEADER = "ERR12002";
+    private static final String INVALID_AUTHORIZATION_HEADER = "ERR12003";
+    private static final String INVALID_BASIC_CREDENTIALS = "ERR12004";
+    private static final String JSON_PROCESSING_EXCEPTION = "ERR12005";
+    private static final String CLIENT_NOT_FOUND = "ERR12014";
+    private static final String UNAUTHORIZED_CLIENT = "ERR12007";
+    private static final String INVALID_AUTHORIZATION_CODE = "ERR12008";
+    private static final String GENERIC_EXCEPTION = "ERR10014";
+    private static final String RUNTIME_EXCEPTION = "ERR10010";
+    private static final String USERNAME_REQUIRED = "ERR12022";
+    private static final String PASSWORD_REQUIRED = "ERR12023";
+    private static final String INCORRECT_PASSWORD = "ERR12016";
+    private static final String NOT_TRUSTED_CLIENT = "ERR12024";
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
@@ -66,9 +71,11 @@ public class Oauth2TokenPostHandler implements HttpHandler {
         }
         try {
             if("client_credentials".equals(formMap.get("grant_type"))) {
-                exchange.getResponseSender().send(mapper.writeValueAsString(handleClientCredentials(exchange)));
+                exchange.getResponseSender().send(mapper.writeValueAsString(handleClientCredentials(exchange, (String)formMap.get("scope"))));
             } else if("authorization_code".equals(formMap.get("grant_type"))) {
-                exchange.getResponseSender().send(mapper.writeValueAsString(handleAuthorizationCode(exchange, (String)formMap.get("code"))));
+                exchange.getResponseSender().send(mapper.writeValueAsString(handleAuthorizationCode(exchange, (String)formMap.get("code"), (String)formMap.get("scope"))));
+            } else if("password".equals(formMap.get("grant_type"))) {
+                exchange.getResponseSender().send(mapper.writeValueAsString(handlePassword(exchange, (String)formMap.get("username"), (String)formMap.get("password"), (String)formMap.get("scope"))));
             } else {
                 Status status = new Status(UNSUPPORTED_GRANT_TYPE, formMap.get("grant_type"));
                 exchange.setStatusCode(status.getStatusCode());
@@ -85,66 +92,97 @@ public class Oauth2TokenPostHandler implements HttpHandler {
     }
 
     @SuppressWarnings("unchecked")
-    private Map<String, Object> handleClientCredentials(HttpServerExchange exchange) throws ApiException {
-        // get Authorization header.
-        String auth = exchange.getRequestHeaders().getFirst("Authorization");
-        if(auth == null || auth.trim().length() == 0) {
-            throw new ApiException(new Status(MISSING_AUTHORIZATION_HEADER));
-        } else {
-            String basic = auth.substring(0, 5);
-            if("BASIC".equalsIgnoreCase(basic)) {
-                String credentials = auth.substring(6);
-                String clientId;
-                String clientSecret;
-                int pos = credentials.indexOf(':');
-                if(pos == -1) {
-                    credentials = decodeCredentials(credentials);
-                }
-                pos = credentials.indexOf(':');
-                if(pos != -1) {
-                    clientId = credentials.substring(0, pos);
-                    clientSecret = credentials.substring(pos + 1);
+    private Map<String, Object> handleClientCredentials(HttpServerExchange exchange, String scope) throws ApiException {
+        if(logger.isDebugEnabled()) logger.debug("scope = " + scope);
+        Client client = authenticateClient(exchange);
+        if(client != null) {
+            if(scope == null) scope = client.getScope();
+            String jwt;
+            try {
+                jwt = JwtHelper.getJwt(mockCcClaims(client.getClientId(), scope));
+            } catch (Exception e) {
+                throw new ApiException(new Status(GENERIC_EXCEPTION, e.getMessage()));
+            }
+            Map<String, Object> resMap = new HashMap<>();
+            resMap.put("access_token", jwt);
+            resMap.put("token_type", "bearer");
+            resMap.put("expires_in", 600);
+            return resMap;
 
-                    IMap<String, Client> clients = CacheStartupHookProvider.hz.getMap("clients");
-                    Client client = clients.get(clientId);
-                    if(client == null) {
-                        throw new ApiException(new Status(CLIENT_NOT_FOUND, clientId));
-                    } else {
-                        try {
-                            if(HashUtil.validatePassword(clientSecret, client.getClientSecret())) {
-                                String scope = client.getScope();
-                                String jwt;
-                                try {
-                                    jwt = JwtHelper.getJwt(mockCcClaims(clientId, scope));
-                                } catch (Exception e) {
-                                    throw new ApiException(new Status(GENERIC_EXCEPTION, e.getMessage()));
-                                }
+        }
+        return new HashMap<>(); // return an empty hash map. this is actually not reachable at all.
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> handleAuthorizationCode(HttpServerExchange exchange, String code, String scope) throws ApiException {
+        if(logger.isDebugEnabled()) logger.debug("code = " + code + " scope = " + scope);
+        Client client = authenticateClient(exchange);
+        if(client != null) {
+            String userId = (String)CacheStartupHookProvider.hz.getMap("codes").remove(code);
+            if(userId != null) {
+                IMap<String, User> users = CacheStartupHookProvider.hz.getMap("users");
+                User user = users.get(userId);
+                if(scope == null) scope = client.getScope();
+                String jwt;
+                try {
+                    jwt = JwtHelper.getJwt(mockAcClaims(client.getClientId(), scope, userId, user.getUserType().toString()));
+                } catch (Exception e) {
+                    throw new ApiException(new Status(GENERIC_EXCEPTION, e.getMessage()));
+                }
+                Map<String, Object> resMap = new HashMap<>();
+                resMap.put("access_token", jwt);
+                resMap.put("token_type", "bearer");
+                resMap.put("expires_in", 600);
+                return resMap;
+            } else {
+                throw new ApiException(new Status(INVALID_AUTHORIZATION_CODE, code));
+            }
+        }
+        return new HashMap<>(); // return an empty hash map. this is actually not reachable at all.
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> handlePassword(HttpServerExchange exchange, String userId, String password, String scope) throws ApiException {
+        if(logger.isDebugEnabled()) logger.debug("userId = " + userId + " scope = " + scope);
+        Client client = authenticateClient(exchange);
+        if(client != null) {
+            // authenticate user with credentials
+            if(userId != null) {
+                if(password != null) {
+                    IMap<String, User> users = CacheStartupHookProvider.hz.getMap("users");
+                    User user = users.get(userId);
+                    // match password
+                    try {
+                        if(HashUtil.validatePassword(password, user.getPassword())) {
+                            // make sure that client is trusted
+                            if(client.getClientType() == Client.ClientTypeEnum.TRUSTED) {
+                                if(scope == null) scope = client.getScope(); // use the default scope defined in client if scope is not passed in
+                                String jwt = JwtHelper.getJwt(mockAcClaims(client.getClientId(), scope, userId, user.getUserType().toString()));
                                 Map<String, Object> resMap = new HashMap<>();
                                 resMap.put("access_token", jwt);
                                 resMap.put("token_type", "bearer");
                                 resMap.put("expires_in", 600);
                                 return resMap;
                             } else {
-                                throw new ApiException(new Status(UNAUTHORIZED_CLIENT));
+                                throw new ApiException(new Status(NOT_TRUSTED_CLIENT));
                             }
-                        } catch ( NoSuchAlgorithmException | InvalidKeySpecException e) {
-                            logger.error("Exception:", e);
-                            throw new ApiException(new Status(RUNTIME_EXCEPTION));
+                        } else {
+                            throw new ApiException(new Status(INCORRECT_PASSWORD));
                         }
+                    } catch (NoSuchAlgorithmException | InvalidKeySpecException | JoseException e) {
+                        throw new ApiException(new Status(GENERIC_EXCEPTION, e.getMessage()));
                     }
                 } else {
-                    throw new ApiException(new Status(INVALID_BASIC_CREDENTIALS, credentials));
+                    throw new ApiException(new Status(PASSWORD_REQUIRED));
                 }
             } else {
-                throw new ApiException(new Status(INVALID_AUTHORIZATION_HEADER, auth));
+                throw new ApiException(new Status(USERNAME_REQUIRED));
             }
         }
+        return new HashMap<>(); // return an empty hash map. this is actually not reachable at all.
     }
 
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> handleAuthorizationCode(HttpServerExchange exchange, String code) throws ApiException {
-        if(logger.isDebugEnabled()) logger.debug("code = " + code);
-        // get Authorization header.
+    private Client authenticateClient(HttpServerExchange exchange) throws ApiException {
         String auth = exchange.getRequestHeaders().getFirst("Authorization");
         if(auth == null || auth.trim().length() == 0) {
             throw new ApiException(new Status(MISSING_AUTHORIZATION_HEADER));
@@ -162,7 +200,6 @@ public class Oauth2TokenPostHandler implements HttpHandler {
                 if(pos != -1) {
                     clientId = credentials.substring(0, pos);
                     clientSecret = credentials.substring(pos + 1);
-
                     IMap<String, Client> clients = CacheStartupHookProvider.hz.getMap("clients");
                     Client client = clients.get(clientId);
                     if(client == null) {
@@ -170,25 +207,7 @@ public class Oauth2TokenPostHandler implements HttpHandler {
                     } else {
                         try {
                             if(HashUtil.validatePassword(clientSecret, client.getClientSecret())) {
-                                String userId = (String)CacheStartupHookProvider.hz.getMap("codes").remove(code);
-                                if(userId != null) {
-                                    IMap<String, User> users = CacheStartupHookProvider.hz.getMap("users");
-                                    User user = users.get(userId);
-                                    String scope = client.getScope();
-                                    String jwt;
-                                    try {
-                                        jwt = JwtHelper.getJwt(mockAcClaims(clientId, scope, userId, user.getUserType().toString()));
-                                    } catch (Exception e) {
-                                        throw new ApiException(new Status(GENERIC_EXCEPTION, e.getMessage()));
-                                    }
-                                    Map<String, Object> resMap = new HashMap<>();
-                                    resMap.put("access_token", jwt);
-                                    resMap.put("token_type", "bearer");
-                                    resMap.put("expires_in", 600);
-                                    return resMap;
-                                } else {
-                                    throw new ApiException(new Status(INVALID_AUTHORIZATION_CODE, code));
-                                }
+                                return client;
                             } else {
                                 throw new ApiException(new Status(UNAUTHORIZED_CLIENT));
                             }
@@ -206,8 +225,7 @@ public class Oauth2TokenPostHandler implements HttpHandler {
         }
     }
 
-
-    public JwtClaims mockCcClaims(String clientId, String scopeString) {
+    private JwtClaims mockCcClaims(String clientId, String scopeString) {
         JwtClaims claims = JwtHelper.getDefaultJwtClaims();
         claims.setClaim("client_id", clientId);
         List<String> scope = Arrays.asList(scopeString.split("\\s+"));
@@ -215,7 +233,7 @@ public class Oauth2TokenPostHandler implements HttpHandler {
         return claims;
     }
 
-    public JwtClaims mockAcClaims(String clientId, String scopeString, String userId, String userType) {
+    private JwtClaims mockAcClaims(String clientId, String scopeString, String userId, String userType) {
         JwtClaims claims = JwtHelper.getDefaultJwtClaims();
         claims.setClaim("user_id", userId);
         claims.setClaim("user_type", userType);
@@ -225,7 +243,7 @@ public class Oauth2TokenPostHandler implements HttpHandler {
         return claims;
     }
 
-    public static String decodeCredentials(String cred) {
+    private static String decodeCredentials(String cred) {
         return new String(org.apache.commons.codec.binary.Base64.decodeBase64(cred), UTF_8);
     }
 }
--- token/src/test/java/com/networknt/oauth/token/handler/Oauth2TokenPostHandlerTest.java
+++ token/src/test/java/com/networknt/oauth/token/handler/Oauth2TokenPostHandlerTest.java
@@ -3,9 +3,11 @@ package com.networknt.oauth.token.handler;
 import com.networknt.config.Config;
 import com.networknt.status.Status;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -172,6 +174,115 @@ public class Oauth2TokenPostHandlerTest {
         Status status = Config.getInstance().getMapper().readValue(response.getEntity().getContent(), Status.class);
         Assert.assertNotNull(status);
         Assert.assertEquals("ERR12003", status.getCode());
+    }
+
+    @Test
+    public void testPasswordToken() throws Exception {
+        String url = "http://localhost:6882/oauth2/token";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Authorization", "Basic " + encodeCredentials("f7d42348-c647-4efb-a52d-4c5787421e72", "f6h1FTI8Q3-7UScPZDzfXA"));
+
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("grant_type", "password"));
+        urlParameters.add(new BasicNameValuePair("username", "admin"));
+        urlParameters.add(new BasicNameValuePair("password", "123456"));
+        urlParameters.add(new BasicNameValuePair("scope", "overwrite.r"));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
+        HttpResponse response = client.execute(httpPost);
+        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+        String body = EntityUtils.toString(response.getEntity());
+        logger.debug("response body = " + body);
+        Assert.assertTrue(body.indexOf("access_token") > 0);
+    }
+
+    @Test
+    public void testPasswordGrantEmptyUsername() throws Exception {
+        String url = "http://localhost:6882/oauth2/token";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Authorization", "Basic " + encodeCredentials("f7d42348-c647-4efb-a52d-4c5787421e72", "f6h1FTI8Q3-7UScPZDzfXA"));
+
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("grant_type", "password"));
+        urlParameters.add(new BasicNameValuePair("password", "123456"));
+        urlParameters.add(new BasicNameValuePair("scope", "overwrite.r"));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
+        try {
+            CloseableHttpResponse response = client.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            String body = IOUtils.toString(response.getEntity().getContent(), "utf8");
+            Assert.assertEquals(400, statusCode);
+            if(statusCode == 400) {
+                Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+                Assert.assertNotNull(status);
+                Assert.assertEquals("ERR12022", status.getCode());
+                Assert.assertEquals("USERNAME_REQUIRED", status.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testPasswordGrantEmptyPassword() throws Exception {
+        String url = "http://localhost:6882/oauth2/token";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Authorization", "Basic " + encodeCredentials("f7d42348-c647-4efb-a52d-4c5787421e72", "f6h1FTI8Q3-7UScPZDzfXA"));
+
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("grant_type", "password"));
+        urlParameters.add(new BasicNameValuePair("username", "admin"));
+        urlParameters.add(new BasicNameValuePair("scope", "overwrite.r"));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
+        try {
+            CloseableHttpResponse response = client.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            String body = IOUtils.toString(response.getEntity().getContent(), "utf8");
+            Assert.assertEquals(400, statusCode);
+            if(statusCode == 400) {
+                Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+                Assert.assertNotNull(status);
+                Assert.assertEquals("ERR12023", status.getCode());
+                Assert.assertEquals("PASSWORD_REQUIRED", status.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testNotTrustedClient() throws Exception {
+        String url = "http://localhost:6882/oauth2/token";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("Authorization", "Basic " + encodeCredentials("6e9d1db3-2feb-4c1f-a5ad-9e93ae8ca59d", "f6h1FTI8Q3-7UScPZDzfXA"));
+
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("grant_type", "password"));
+        urlParameters.add(new BasicNameValuePair("username", "admin"));
+        urlParameters.add(new BasicNameValuePair("password", "123456"));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
+        try {
+            CloseableHttpResponse response = client.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            String body = IOUtils.toString(response.getEntity().getContent(), "utf8");
+            Assert.assertEquals(400, statusCode);
+            if(statusCode == 400) {
+                Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+                Assert.assertNotNull(status);
+                Assert.assertEquals("ERR12024", status.getCode());
+                Assert.assertEquals("NOT_TRUSTED_CLIENT", status.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
